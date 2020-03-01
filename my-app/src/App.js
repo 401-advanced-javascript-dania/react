3c505798e0cd00bd933f79a96dbc9ab39e51fa9b
daniaalrababa9 <rababadania@gmail.com>
2020-03-01 14:39:32 +0200

    fix the eslint error and do the UML

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './modules/header.js'
-import Footer from './modules/footer.js'
-import Main from'./modules/main.js'
+import Header from './modules/header.js';
+import Footer from './modules/footer.js';
+import Main from'./modules/main.js';
 import './app.scss';
 class App extends React.Component{
   render(){
@@ -11,7 +11,7 @@ class App extends React.Component{
         <Main/>
         <Footer/>
       </React.Fragment>
-    )
+    );
   }
 }
 export default App;
--- my-app/src/modules/footer.js
+++ my-app/src/modules/footer.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 const Footer = () =>{
-    return(
-      <footer>
+  return(
+    <footer>
         done by me :-)
-      </footer>
-    )
-  }
-  export default Footer;
+    </footer>
+  );
+};
+export default Footer;
